836db65f4361e99fddd00071793b58522ccd452c
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-11-09 12:16:23 -0300

    inicio clase 2

---
+++ clase2_Control_de_flujo_Funciones/ejercicios.js
@@ -0,0 +1,57 @@
+/* 
+**Ejercicio 1: Estructuras Condicionales**
+1. Escribe un programa que verifique si un número es positivo, negativo o cero. Pide al usuario que ingrese un número y muestra el resultado.
+ */
+
+/* 
+2. Crea un programa que determine si un estudiante aprobó o reprobó un examen. Pide al usuario que ingrese su calificación y establece un umbral de aprobación.
+ */
+
+
+
+
+/* 
+**Ejercicio 2: Bucles**
+1. Escribe un programa que imprima la tabla de multiplicar para un número dado. Pide al usuario que ingrese un número y muestra la tabla hasta el 10.
+ */
+
+/* 
+2. Crea un juego de adivinanzas en el que la computadora genera un número aleatorio y el usuario tiene que adivinarlo. Proporciona pistas si la suposición es demasiado alta o demasiado baja. Continúa hasta que se adivine el número correcto.
+ */
+
+
+
+
+
+/* 
+**Ejercicio 3: Funciones y Ámbito**
+1. Crea una función que calcule el área de un círculo. El usuario debe proporcionar el radio, y la función debe devolver el área.
+ */
+
+/* 
+2. Escribe un programa que utilice una función para determinar si un año dado es bisiesto. Pide al usuario que ingrese un año y muestra el resultado.
+ */
+
+
+
+
+/* 
+**Ejercicio 4: Funciones Recursivas**
+1. Implementa una función recursiva para calcular el factorial de un número dado. Pide al usuario que ingrese un número y muestra el resultado.
+ */
+
+/* 
+2. Crea un programa que utilice la recursividad para encontrar el término n-ésimo de la secuencia de Fibonacci. Pide al usuario que ingrese un número y muestra el resultado.
+ */
+
+
+
+
+/* 
+**Ejercicio 5: Parámetros y Valores de Retorno de Funciones**
+1. Crea una función que tome dos parámetros (base y exponente) y calcule el resultado de elevar la base a la potencia del exponente.
+ */
+
+/* 
+2. Escribe un programa que utilice una función para verificar si una cadena dada es un palíndromo. Pide al usuario que ingrese una cadena y muestra el resultado.
+ */
