f0bd3ca952b341fd42b048e37108f9020647a4a9
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-12-09 11:52:43 -0300

    ejercicios clase 6 promesas

---
+++ clase6/ejercicios.js
@@ -0,0 +1,13 @@
+// Exercise 1
+// Use Promise.resolve(value) to create a promise that will resolve with the value 3.
+
+// Exercise 2
+// Use Promise.reject(error) to create a promise that will reject with the string "Boo!"
+
+// Exercise 3
+// You have the outline of a function makePromiseWithConstructor(itShouldResolve)
+
+// Use the Promise constructor to create a promise that will:
+
+// resolve if itShouldResolve is truthy
+// reject if itShouldResolve is falsy
